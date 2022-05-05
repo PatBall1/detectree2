0dc91263762fe23f9dc2c1d81b17a752e3b20a7f
James Ball <jgcb3@sherwood.ocl.cl.cam.ac.uk>
2022-05-05 19:14:46 +0100

    try package

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="src",
+    name="detectree2",
     version="0.0.1",
     author="James G. C. Ball",
     author_email="ball.jgc@gmail.com.com",
     description="Detectree packaging",
     url="https://github.com/PatBall1/detectree2",
+    package_dir={"": "src"},
     packages=find_packages(),
     test_suite="src.tests.test_all.suite",
     install_requires=[
