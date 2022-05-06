b7721bd21a771751803e82583620afdd65d5270e
James Ball <jgcb3@sherwood.ocl.cl.cam.ac.uk>
2022-05-06 18:00:48 +0100

    deletes

--- src/R/README.md
+++
@@ -1,5 +0,0 @@
-# R Scripts
-
-Some scripts remain in R, including the script to stiched together the tiled crown outputs.
-
-There should be converted to Python!
--- src/tests/test_data.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-class TestCase(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-
-
-suite = unittest.TestLoader().loadTestsFromTestCase(TestCase)
