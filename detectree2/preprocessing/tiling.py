9afeeeb63f00262e9c46acf6e6dd2310cedda66c
Ball JGC <ball.jgc@gmail.com>
2022-06-19 22:50:23 +0000

    folder organising function

--- detectree2/preprocessing/tiling.py
+++ detectree2/preprocessing/tiling.py
@@ -360,6 +360,68 @@ def tile_data_train(
                 print("ValueError: Cannot write empty DataFrame to file.")
                 continue
 
+def to_traintest_folders(tiles_folder="./", out_folder="./data/", test_frac=0.2, folds=1):
+  """
+  To send tiles to training (validation) and test folder
+  """
+
+  Path(out_folder + "train").mkdir(parents=True, exist_ok=True)
+  Path(out_folder + "test").mkdir(parents=True, exist_ok=True)
+
+  # First split between train and test
+  #split = np.array([4, 1])
+  split = np.array([(1-test_frac), test_frac])
+  summed = np.sum(split)
+  percs = 100 * split / summed
+  percs = np.cumsum(percs)
+
+  filenames = glob.glob(tiles_folder + "*.png")
+  jsonnames = glob.glob(tiles_folder + "*.geojson")
+
+  stemname = Path(filenames[0]).stem.split("_", 1)[0]
+
+  indices = [item.split("_", 1)[-1].split(".", 1)[0] for item in filenames]
+
+  num = list(range(0, len(indices)))
+  random.shuffle(num)
+
+
+  for i in range(0, len(indices)):
+      #print(i)
+      if num[i] < np.percentile(num, percs[0]):
+          shutil.copy(filenames[i], out_folder + "train")
+          shutil.copy("./" + stemname + "_" + indices[i] + ".geojson", "./data/train/")
+      # elif num[i] < np.percentile(num, percs[1]):
+      #    shutil.copy(filenames[i], "./data/val/")
+      #    shutil.copy("./data/" + stemname + "_" + indices[i] + ".geojson", "./data/val/")
+      else:
+          shutil.copy(filenames[i], out_folder + "test")
+          shutil.copy("./" + stemname + "_" + indices[i] + ".geojson", "./data/test/")
+
+  filenames = glob.glob(out_folder + "/train/*.png")
+  jsonnames = glob.glob(out_folder + "/train/*.geojson")
+
+  stemname = Path(filenames[0]).stem.split("_", 1)[0]
+
+  indices = [item.split("_", 1)[-1].split(".", 1)[0] for item in filenames]
+
+  random.shuffle(indices)
+  ind_split = np.array_split(indices, folds)
+
+  for i in range(0, folds):
+      Path(out_folder + "/train/fold_" + str(i + 1) + "/").mkdir(parents=True, exist_ok=True)
+      for ind in ind_split[i]:
+          #print(ind)
+          shutil.move(
+              out_folder + "train/" + stemname + "_" + ind + ".png",
+              out_folder + "train/fold_" + str(i + 1) + "/",
+          )
+          shutil.move(
+              out_folder + "train/" + stemname + "_" + ind + ".geojson",
+              out_folder + "train/fold_" + str(i + 1) + "/",
+          )
+
+
 
 if __name__ == "__main__":
     # Right let's test this first with Sepilok 10cm resolution, then I need to try it with 50cm resolution.
