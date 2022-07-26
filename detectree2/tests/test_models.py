965f9f88446335194e27c6975c89bd84b973617f
Matthew Archer <ma595@cam.ac.uk>
2022-07-26 19:03:44 +0100

    first attempt at some simple tests

--- detectree2/tests/test_models.py
+++ detectree2/tests/test_models.py
@@ -1,10 +1,50 @@
 import unittest
 
+import cv2
+
+# import common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.structures import BoxMode
+
 
 class TestCase(unittest.TestCase):
 
     def test_upper(self):
         self.assertEqual("foo".upper(), "FOO")
 
+    def test_detectron2(self):
+        # !wget http://images.cocodataset.org/val2017/000000439715.jpg -O input.jpg
+        im = cv2.imread("./input/input.jpg")
 
+        # Create config
+        cfg = get_cfg()
+        
+        cfg.MODEL.DEVICE = 'cpu'
+
+        cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
+
+        #cfg.merge_file = 'https://github.com/facebookresearch/detectron2/blob/master/configs/COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml'
+        #cfg.merge_from_file("./detectron2/configs/COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")
+
+        # set threshold for this model
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  
+
+        #cfg.MODEL.WEIGHTS = "detectron2://COCO-Detection/faster_rcnn_R_101_FPN_3x/137851257/model_final_f6e8b1.pkl"
+
+        # Here we just get the pre-trained weights straight from facebook hosting website
+
+        cfg.MODEL.WEIGHTS = 'https://dl.fbaipublicfiles.com/detectron2/COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x/138205316/model_final_a3ec72.pkl'
+
+        # Create predictor
+        predictor = DefaultPredictor(cfg)
+
+        # Make prediction
+        outputs = predictor(im)
+        
+        self.assertEqual(1,1)
+        
 suite = unittest.TestLoader().loadTestsFromTestCase(TestCase)
--- detectree2/tests/test_preprocessing.py
+++ detectree2/tests/test_preprocessing.py
@@ -1,4 +1,7 @@
 import unittest
+import os
+import rasterio
+import geopandas as gpd
 
 
 class TestCase(unittest.TestCase):
@@ -6,5 +9,32 @@ class TestCase(unittest.TestCase):
     def test_upper(self):
         self.assertEqual("foo".upper(), "FOO")
 
+    def test_tiling(self):
+        ### SEPILOK (East/West)
+        #site_path = "/content/drive/Shareddrives/detectree2/data/Sepilok"
+        site_path = "../data/sepilok"
+        img_path = os.path.join(site_path, "RCD105_MA14_21_orthomosaic_20141023_reprojected_full_res.tif")
+        crown_path = os.path.join(site_path, "crowns/SepilokEast.gpkg")
+        # crown_path = os.path.join(site_path, "crowns/SepilokWest.gpkg")
+     
+        # Read in the tiff file
+        data = rasterio.open(img_path)
+        # Read in crowns (then filter by an attribute?)
+        crowns = gpd.read_file(crown_path)
+        crowns = crowns.to_crs(data.crs.data)
+
+        # Set tiling parameters
+        buffer = 15
+        tile_width = 40
+        tile_height = 40
+        threshold = 0.5 
+      
+        from detectree2.preprocessing.tiling import tile_data_train
+        
+        out_dir = "./out/"
+ 
+        tile_data_train(data, out_dir, buffer, tile_width, tile_height, crowns, threshold)
+        
+        return True
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestCase)
