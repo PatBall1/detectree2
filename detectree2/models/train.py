fad5c212b77a5bdbc3c205dbb4ebaeb783481138
WangLuran <94464527+WangLuran@users.noreply.github.com>
2022-08-08 16:12:12 +0100

    train.py change (#25)
    * Update train.py
    
    * Update train.py
    
    Add early stopping using AP50 values and set up the hyper_parameters in Tomas's report.
    
    Co-authored-by: James Ball <37094972+PatBall1@users.noreply.github.com>

--- detectree2/models/train.py
+++ detectree2/models/train.py
@@ -20,6 +20,8 @@ import detectron2.utils.comm as comm
 from detectron2.structures import BoxMode
 from detectron2.engine.hooks import HookBase
 from detectron2.evaluation import COCOEvaluator
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.utils.events import EventStorage, get_event_storage
 from detectron2.evaluation.coco_evaluation import instances_to_coco_json
 from detectron2.data import (
     MetadataCatalog,
@@ -48,10 +50,15 @@ class LossEvalHook(HookBase):
         data loader
     """
 
-    def __init__(self, eval_period, model, data_loader):
+    def __init__(self, eval_period, model, data_loader, patience):
         self._model = model
         self._period = eval_period
         self._data_loader = data_loader
+        self.patience = patience
+        self.iter = 0
+        self.max_ap = 0
+        self.best_iter = 0
+
 
     def _do_loss_eval(self):
         """Copying inference_on_dataset from evaluator.py
@@ -82,16 +89,19 @@ class LossEvalHook(HookBase):
                                                      (total - idx - 1)))
                 log_every_n_seconds(
                     logging.INFO,
-                    "Loss on Validation  done {}/{}. {:.4f} s / img. ETA={}".
-                    format(idx + 1, total, seconds_per_img, str(eta)),
+                    "Loss on Validation  done {}/{}. {:.4f} s / img. ETA={}".format(
+                        idx + 1, total, seconds_per_img, str(eta)),
                     n=5,
                 )
             loss_batch = self._get_loss(inputs)
             losses.append(loss_batch)
         mean_loss = np.mean(losses)
+        AP = self.trainer.test(self.trainer.cfg, self.trainer.model)['segm']['AP50']
+        self.trainer.APs.append(AP)
         self.trainer.storage.put_scalar("validation_loss", mean_loss)
+        self.trainer.storage.put_scalar("validation_ap", AP)
         comm.synchronize()
-
+    
         return losses
 
     def _get_loss(self, data):
@@ -106,19 +116,33 @@ class LossEvalHook(HookBase):
         #
         metrics_dict = self._model(data)
         metrics_dict = {
-            k:
-            v.detach().cpu().item() if isinstance(v, torch.Tensor) else float(v)
+            k: v.detach().cpu().item() if isinstance(v, torch.Tensor) else float(v)
             for k, v in metrics_dict.items()
         }
         total_losses_reduced = sum(loss for loss in metrics_dict.values())
         return total_losses_reduced
-
+    
     def after_step(self):
         next_iter = self.trainer.iter + 1
         is_final = next_iter == self.trainer.max_iter
         if is_final or (self._period > 0 and next_iter % self._period == 0):
             self._do_loss_eval()
+            if self.max_ap < self.trainer.APs[-1]:
+                self.iter = 0
+                self.max_ap = self.trainer.APs[-1]
+                self.trainer.checkpointer.save('model_' + str(len(self.trainer.APs)))
+                self.best_iter = self.trainer.iter
+            else:
+                self.iter += 1
+          if self.iter == self.patience:
+              self.trainer.early_stop = True
+              print("Early stopping occurs in iter {}, max ap is {}".format(self.best_iter, self.max_ap))
         self.trainer.storage.put_scalars(timetest=12)
+       
+    def after_train(self):
+        index = self.trainer.APs.index(min(self.trainer.APs)) + 1
+        self.trainer.checkpointer.load(self.trainer.cfg.OUTPUT_DIR + '/model_' + str(index) + '.pth')
+
 
 
 # See https://jss367.github.io/data-augmentation-in-detectron2.html for data augmentation advice
@@ -131,14 +155,63 @@ class MyTrainer(DefaultTrainer):
     Returns:
         _type_: _description_
     """
-
+    def __init__(self, cfg, patience):
+        self.patience = patience
+        super().__init__(cfg)
+  
+    def train(self):
+        """Run training.
+        
+        Args:
+            start_iter, max_iter (int): See docs above
+        
+        Returns:
+            OrderedDict of results, if evaluation is enabled. Otherwise None.
+        """
+        
+        start_iter = self.start_iter
+        max_iter = self.max_iter
+        logger = logging.getLogger(__name__)
+        logger.info("Starting training from iteration {}".format(start_iter))
+    
+        self.iter = self.start_iter = start_iter
+        self.max_iter = max_iter
+        self.early_stop = False
+        self.APs = []
+    
+        with EventStorage(start_iter) as self.storage:
+            try:
+                self.before_train()
+                for self.iter in range(start_iter, max_iter):
+                    self.before_step()
+                    self.run_step()
+                    self.after_step()
+                    if self.early_stop:
+                      break
+                # self.iter == max_iter can be used by `after_train` to
+                # tell whether the training successfully finished or failed
+                # due to exceptions.
+                self.iter += 1
+            except Exception:
+                logger.exception("Exception during training:")
+                raise
+            finally:
+                self.after_train()
+        if len(self.cfg.TEST.EXPECTED_RESULTS) and comm.is_main_process():
+            assert hasattr(
+                self, "_last_eval_results"
+            ), "No evaluation results obtained during training!"
+            verify_results(self.cfg, self._last_eval_results)
+            return self._last_eval_results
+    
+    
     @classmethod
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
         if output_folder is None:
             os.makedirs("eval_2", exist_ok=True)
             output_folder = "eval_2"
         return COCOEvaluator(dataset_name, cfg, True, output_folder)
-
+  
     def build_hooks(self):
         hooks = super().build_hooks()
         hooks.insert(
@@ -147,44 +220,44 @@ class MyTrainer(DefaultTrainer):
                 self.cfg.TEST.EVAL_PERIOD,
                 self.model,
                 build_detection_test_loader(self.cfg, self.cfg.DATASETS.TEST[0],
-                                            DatasetMapper(self.cfg, True)),
+                DatasetMapper(self.cfg, True)),
+                self.patience,
             ),
         )
         return hooks
-
+  
     def build_train_loader(cls, cfg):
         """_summary_
-
-        Args:
-            cfg (_type_): _description_
-
-        Returns:
-            _type_: _description_
-        """
-        return build_detection_train_loader(
-            cfg,
-            mapper=DatasetMapper(
-                cfg,
-                is_train=True,
-                augmentations=[
-                    T.Resize((800, 800)), # is this necessary/helpful?
-                    T.RandomBrightness(0.8, 1.8),
-                    T.RandomContrast(0.6, 1.3),
-                    T.RandomSaturation(0.8, 1.4),
-                    T.RandomRotation(angle=[90, 90], expand=False),
-                    T.RandomLighting(0.7),
-                    T.RandomFlip(prob=0.4, horizontal=True, vertical=False),
-                    T.RandomFlip(prob=0.4, horizontal=False, vertical=True),
-                ],
-            ),
-        )
+          Args:
+              cfg (_type_): _description_
+  
+          Returns:
+              _type_: _description_
+          """
+          return build_detection_train_loader(
+              cfg,
+              mapper=DatasetMapper(
+                  cfg,
+                  is_train=True,
+                  augmentations=[
+                      T.Resize((800, 800)), # is this necessary/helpful?
+                      T.RandomBrightness(0.8, 1.8),
+                      T.RandomContrast(0.6, 1.3),
+                      T.RandomSaturation(0.8, 1.4),
+                      T.RandomRotation(angle=[90, 90], expand=False),
+                      T.RandomLighting(0.7),
+                      T.RandomFlip(prob=0.4, horizontal=True, vertical=False),
+                      T.RandomFlip(prob=0.4, horizontal=False, vertical=True),
+                  ],
+              ),
+          )
 
 
 def get_tree_dicts(directory, classes=None):
     """
     directory points to files
     classes signifies which column (if any) corresponds to the class labels
-  """
+    """
     # filepath = '/content/drive/MyDrive/forestseg/paracou_data/Panayiotis_Outputs/220303_AllSpLabelled.gpkg'
     # datagpd = gpd.read_file(filepath)
     # List_Genus = datagpd.Genus_Species.to_list()
@@ -335,33 +408,45 @@ def load_json_arr(json_path):
     return lines
 
 
-def setup_cfg(
-        base_model="COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml",
-        trains=("trees_train",),
-        tests=("trees_val",),
-        update_model=None,
-        workers=2,
-        ims_per_batch=2,
-        base_lr=0.0003,
-        max_iter=1000,
-        num_classes=1,
-        eval_period=100,
-        out_dir="/content/drive/Shareddrives/detectree2/train_outputs"):
+def setup_cfg(base_model="COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml",
+              trains=("trees_train",),
+              tests=("trees_val",),
+              update_model=None,
+              workers=2,
+              ims_per_batch=2,
+              gamma = 0.1,
+              backbone_freeze = 3,
+              warm_iter = 120,
+              momentum = 0.9,
+              batch_size_per_im = 1024,
+              base_lr=0.001,
+              max_iter=1000,
+              num_classes=1,
+              eval_period=100,
+              out_dir="/content/drive/Shareddrives/detectree2/train_outputs"):
     """
-  To set up config object
-  """
+    To set up config object
+    """
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file(base_model))
-    cfg.DATASETS.TRAIN = trains
+    cfg.DATASETS.TRAIN = trains 
     cfg.DATASETS.TEST = tests
     cfg.DATALOADER.NUM_WORKERS = workers
+    #cfg.SOLVER.IMS_PER_BATCH = ims_per_batch
+    cfg.SOLVER.GAMMA = gamma
+    cfg.MODEL.BACKBONE.FREEZE_AT = backbone_freeze
+    cfg.SOLVER.WARMUP_ITERS = warm_iter
+    cfg.SOLVER.MOMENTUM = momentum
+    #cfg.MODEL.RPN.BATCH_SIZE_PER_IMAGE = batch_size_per_im
+    #cfg.SOLVER.WEIGHT_DECAY = 0.001
+    cfg.SOLVER.BASE_LR = base_lr
     cfg.OUTPUT_DIR = out_dir
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
     if update_model is not None:
-        cfg.MODEL.WEIGHTS = update_model    # DOESN'T WORK
+        cfg.MODEL.WEIGHTS = update_model # DOESN'T WORK
     else:
         cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(base_model)
-
+    
     cfg.SOLVER.IMS_PER_BATCH = ims_per_batch
     cfg.SOLVER.BASE_LR = base_lr
     cfg.SOLVER.MAX_ITER = max_iter
@@ -370,63 +455,79 @@ def setup_cfg(
     return cfg
 
 
-def predictions_on_data(directory=None,
-                        predictor=DefaultTrainer,
-                        trees_metadata=None,
-                        save=True,
-                        scale=1,
-                        geos_exist=True,
-                        num_predictions=0):
+def get_filenames(directory):
     """
-  Prediction produced from a test folder and outputted to predictions folder
-  """
+    Used to get the file names if no geojson is present, i.e allows for predictions
+    where no delinations have been manually produced
+    """
+    dataset_dicts = []
+    for filename in [file for file in os.listdir(directory)]:
+        file = {}
+        filename = os.path.join(directory, filename)
+        file["file_name"] = filename
+    
+        dataset_dicts.append(file)
+    return dataset_dicts
 
-    test_location = directory + "test"
-    pred_dir = directory + "predictions"
 
-    Path(pred_dir).mkdir(parents=True, exist_ok=True)
-
-    if geos_exist:
-        dataset_dicts = get_tree_dicts(test_location)
-    else:
-        dataset_dicts = get_filenames(test_location)
-
-    # Works out if all items in folder should be predicted on
-    if num_predictions == 0:
-        num_to_pred = len(dataset_dicts)
-    else:
-        num_to_pred = num_predictions
-
-    for d in random.sample(dataset_dicts, num_to_pred):
-        img = cv2.imread(d["file_name"])
-        # cv2_imshow(img)
-        outputs = predictor(img)
-        v = Visualizer(img[:, :, ::-1],
-                       metadata=trees_metadata,
-                       scale=scale,
-                       instance_mode=ColorMode.SEGMENTATION
-                      )    # remove the colors of unsegmented pixels
-        v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        image = cv2.cvtColor(v.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB)
-        display(Image.fromarray(image))
-
-        ### Creating the file name of the output file
-        file_name_path = d["file_name"]
-        file_name = os.path.basename(
-            os.path.normpath(file_name_path)
-        )    #Strips off all slashes so just final file name left
-        file_name = file_name.replace("png", "json")
-
-        output_file = pred_dir + "/Prediction_" + file_name
-        print(output_file)
-
-        if save:
-            ## Converting the predictions to json files and saving them in the specfied output file.
-            evaluations = instances_to_coco_json(outputs["instances"].to("cpu"),
-                                                 d["file_name"])
-            with open(output_file, "w") as dest:
-                json.dump(evaluations, dest)
-
+def predictions_on_data(directory = None,
+                        predictor = DefaultTrainer,
+                        trees_metadata = None,
+                        save = True,
+                        scale = 1,
+                        geos_exist = True,
+                        num_predictions = 0
+    ):
+    """
+    Prediction produced from a test folder and outputted to predictions folder
+    """
+  
+        test_location = directory + "test"
+        pred_dir = directory + "predictions"
+    
+        Path(pred_dir).mkdir(parents=True, exist_ok=True)
+    
+        if geos_exist:
+            dataset_dicts = get_tree_dicts(test_location)
+        else:
+            dataset_dicts = get_filenames(test_location)
+    
+        # Works out if all items in folder should be predicted on
+        if num_predictions == 0:
+            num_to_pred = len(dataset_dicts)
+        else:
+            num_to_pred = num_predictions
+    
+        for d in random.sample(dataset_dicts, num_to_pred):
+            img = cv2.imread(d["file_name"])
+            # cv2_imshow(img)
+            outputs = predictor(img)
+            v = Visualizer(img[:, :, ::-1],
+                           metadata=trees_metadata,
+                           scale=scale,
+                           instance_mode=ColorMode.SEGMENTATION
+                          )    # remove the colors of unsegmented pixels
+            v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+            image = cv2.cvtColor(v.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB)
+            display(Image.fromarray(image))
+    
+            ### Creating the file name of the output file
+            file_name_path = d["file_name"]
+            file_name = os.path.basename(
+                os.path.normpath(file_name_path)
+            )    #Strips off all slashes so just final file name left
+            file_name = file_name.replace("png", "json")
+    
+            output_file = pred_dir + "/Prediction_" + file_name
+            print(output_file)
+    
+            if save:
+                ## Converting the predictions to json files and saving them in the specfied output file.
+                evaluations = instances_to_coco_json(outputs["instances"].to("cpu"),
+                                                     d["file_name"])
+                with open(output_file, "w") as dest:
+                    json.dump(evaluations, dest)
+  
 
 if __name__ == "__main__":
     train_location = "/content/drive/Shareddrives/detectree2/data/Paracou/tiles/train/"
