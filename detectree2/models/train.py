edc8c3ccdd0a2d8af918128d59db0e209adff99f
WangLuran <94464527+WangLuran@users.noreply.github.com>
2022-09-11 23:00:51 +0100

    Update train.py

--- detectree2/models/train.py
+++ detectree2/models/train.py
@@ -15,6 +15,7 @@ import numpy as np
 import torch
 from detectron2 import model_zoo
 from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.engine import DefaultPredictor, DefaultTrainer, hooks
 from detectron2.config import get_cfg
 from detectron2.data import (DatasetCatalog, DatasetMapper, MetadataCatalog,
                              build_detection_test_loader,
