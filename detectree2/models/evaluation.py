e513f006e07143ad10c4df0412591a99908db13a
Ball JGC <ball.jgc@gmail.com>
2022-09-13 11:58:06 +0000

    geo evaluation

--- detectree2/models/evaluation.py
+++ detectree2/models/evaluation.py
@@ -29,7 +29,7 @@ class Feature:
         Args:
             filename: name of the file within the directory in questions
             directory: the path to the file folder
-            number: a label added to each crown to allow for identifcation
+            number: a label added to each crown to allow for identification
             feature: dictionary containing all the information about a crown
             lidar_filename: the full path to the crown file that overlays with the lidar data
             lidar_img: path to the lidar image of an entire region
@@ -57,7 +57,7 @@ class Feature:
             coord_tuples.append((entry[0], entry[1]))
 
         return coord_tuples
-
+    # TODO: possible to do this straight from georeferenced preds?
     def poly_area(self):
         """Calculates the area of the feature from scaled geojson."""
         polygon = Polygon(self.get_tuple_coords(
@@ -85,6 +85,92 @@ class Feature:
             geo = [{
                 'type': 'Polygon',
                 'coordinates': [self.get_tuple_coords(lidar_coords)]
+            }]
+
+            with rasterio.open(self.lidar_img) as src:
+                out_image, out_transform = mask(src, geo, crop=True)
+            out_meta = src.meta.copy()    # noqa:F841
+
+            # remove all the values that are nodata values and recorded as negatives
+            fixed_array = (out_image[out_image > 0])
+
+            # the lidar data can have missed out areas or have noise meaning
+            # the array is empty hence we will give this feature height 0 so
+            # it is still used in calculating F1 scores in general but ignored
+            # if any height restriction is used
+            if len(fixed_array) != 0:
+                sorted_array = np.sort(fixed_array)
+                self.height = sorted_array[int(len(sorted_array) * 0.5)]
+            else:
+                self.height = 0
+
+class GeoFeature:
+    """Feature class to store.
+
+    Longer class information.
+    """
+
+    def __init__(self, filename, directory, number, feature,
+                 lidar_img, EPSG):    # noqa:N803
+        """Initialise a crown feature with all the required attributes.
+
+        Args:
+            filename: name of the file within the directory in questions
+            directory: the path to the file folder
+            number: a label added to each crown to allow for identification
+            feature: dictionary containing all the information about a crown
+            lidar_img: path to the lidar image of an entire region
+            EPSG: area code of tree location
+        """
+        self.filename = filename
+        self.directory = directory
+        self.number = number
+        self.properties = feature['properties']
+        self.geometry = feature['geometry']
+        self.GIoU = 0
+        self.EPSG = EPSG
+        self.lidar_img = lidar_img
+        self.GIoU_other_feat_num = -1
+        self.poly_area()
+        self.tree_height()
+
+    def get_tuple_coords(self, coords):
+        """Converts coordinates' data structure from a list of lists to a list
+        of tuples."""
+        coord_tuples = []
+
+        for entry in coords:
+            coord_tuples.append((entry[0], entry[1]))
+
+        return coord_tuples
+    # TODO: possible to do this straight from georeferenced preds?
+    def poly_area(self):
+        """Calculates the area of the feature from scaled geojson."""
+        polygon = Polygon(self.get_tuple_coords(
+            self.geometry['coordinates'][0]))
+
+        self.crown_area = polygon.area
+
+    def tree_height(self):
+        """Crops the lidar tif to the features and calculates height
+
+        Calculates the median height to account for
+        error at the boundaries. If no lidar file is inputted than the height is
+        given as 0
+        """
+        if self.lidar_img is None:
+            self.height = 0
+        else:
+            with open(self.filename) as file:
+                geojson = json.load(file)
+
+            # Want coord tuples for the unmoved crown coordinates so using the
+            # lidar copied crown file
+            coords = geojson['features'][
+                self.number]['geometry']['coordinates'][0]
+            geo = [{
+                'type': 'Polygon',
+                'coordinates': [self.get_tuple_coords(coords)]
             }]
 
             with rasterio.open(self.lidar_img) as src:
@@ -142,7 +228,9 @@ def feat_threshold_tests(feature_instance, conf_threshold, area_threshold,
     TW = tile_width
     if valid_feature and border_filter[0]:
         EB = tile_width * border_filter[1]
+        # Go through each coordinate pair in feautre
         for coords in feature_instance.geometry['coordinates'][0]:
+            # if coordinate is out of bounds, skip it
             if (-EB <= coords[0] <= EB or -EB <= coords[1] <= EB
                     or TW - EB <= coords[0] <= TW + EB
                     or TW - EB <= coords[1] <= TW + EB):
@@ -172,6 +260,36 @@ def initialise_feats(
     count = 0
     for feat in feats:
         feat_obj = Feature(file, directory, count, feat, lidar_filename,
+                           lidar_img, EPSG)
+
+        if feat_threshold_tests(feat_obj, conf_threshold, area_threshold,
+                                border_filter, tile_width):
+            all_feats.append(feat_obj)
+            count += 1
+        else:
+            continue
+
+    return all_feats
+
+def initialise_feats2(
+    directory,
+    file,
+    lidar_img,
+    area_threshold,
+    conf_threshold,
+    border_filter,
+    tile_width,
+    EPSG
+):
+    """Creates a list of all the features as objects of the class."""
+    with open(directory + "/" + file) as feat_file:
+        feat_json = json.load(feat_file)
+    feats = feat_json["features"]
+
+    all_feats = []
+    count = 0
+    for feat in feats:
+        feat_obj = Feature(file, directory, count, feat,,
                            lidar_img, EPSG)
 
         if feat_threshold_tests(feat_obj, conf_threshold, area_threshold,
@@ -317,10 +435,11 @@ def site_f1_score(tile_directory=None,
                   IoU_threshold=0.5,
                   min_height=0,
                   max_height=100,
-                  area_fraction_limit=0.0005,
+                  #area_fraction_limit=0.0005,
+                  area_threshold=25,
                   conf_threshold=0,
                   border_filter=tuple,
-                  scaling=list,
+                  #scaling=list,
                   EPSG=None,
                   save=False):
     """Calculating all the intersections of shapes in a pair of files and the
@@ -334,7 +453,7 @@ def site_f1_score(tile_directory=None,
         IoU_threshold: minimum value of IoU such that the intersection can be considered a true positive
         min_height: minimum height of the features to be considered
         max_height: minimum height of the features to be considered
-        area_fraction_limit: proportion of the tile for which crowns with areas less than this will be ignored
+        area_threshold: min crown area to consider in m^2
         conf_threshold: minimun confidence of a predicted feature so that it is considered
         border_filter: bool of whether to remove border crowns, proportion of border to be used
         in relation to tile size
@@ -356,8 +475,10 @@ def site_f1_score(tile_directory=None,
             print(file)
 
             # work out the area threshold to ignore these crowns in the tiles
-            tile_width = get_tile_width(file) * scaling[0]
-            area_threshold = ((tile_width)**2) * area_fraction_limit
+            #tile_width = get_tile_width(file) * scaling[0]
+            #area_threshold = ((tile_width)**2) * area_fraction_limit
+
+            area_threshold = get_tile_width(file)
 
             test_lidar = tile_directory + "/" + file.replace(".geojson", "_geo.geojson")
             all_test_feats = initialise_feats(test_directory, file, test_lidar,
@@ -385,9 +506,9 @@ def site_f1_score(tile_directory=None,
             print("fns:", fns)
             print("")
 
-            total_tps = total_tps + tps
-            total_fps = total_fps + fps
-            total_fns = total_fns + fns
+            total_tps += tps
+            total_fps += fps
+            total_fns += fns
 
     try:
         prec, rec = prec_recall_func(total_tps, total_fps, total_fns)
@@ -399,6 +520,97 @@ def site_f1_score(tile_directory=None,
         print("ZeroDivisionError: Height threshold is too large.")
     return prec, rec, f1_score
 
+
+def site_f1_score2(tile_directory=None,
+                  test_directory=None,
+                  pred_directory=None,
+                  lidar_img=None,
+                  IoU_threshold=0.5,
+                  min_height=0,
+                  max_height=100,
+                  #area_fraction_limit=0.0005,
+                  area_threshold=25,
+                  conf_threshold=0,
+                  border_filter=tuple,
+                  #scaling=list,
+                  EPSG=None,
+                  save=False):
+    """Calculating all the intersections of shapes in a pair of files and the
+    area of the corresponding polygons.
+
+    Args:
+        tile_directory: path to the folder containing all of the tiles
+        test_directory: path to the folder containing just the test files
+        pred_directory: path to the folder containing the predictions and the reprojections
+        lidar_img: path to the lidar image of an entire region
+        IoU_threshold: minimum value of IoU such that the intersection can be considered a true positive
+        min_height: minimum height of the features to be considered
+        max_height: minimum height of the features to be considered
+        area_threshold: min crown area to consider in m^2
+        conf_threshold: minimun confidence of a predicted feature so that it is considered
+        border_filter: bool of whether to remove border crowns, proportion of border to be used
+        in relation to tile size
+        scaling: x and y scaling used when tiling the image
+        EPSG: area code of tree location
+        save: bool to tell program whether the filtered crowns should be saved
+    """
+
+    if EPSG is None:
+        raise ValueError('Set the EPSG value')
+
+    test_entries = os.listdir(test_directory)
+    total_tps = 0
+    total_fps = 0
+    total_fns = 0
+
+    for file in test_entries:
+        if ".geojson" in file:
+            print(file)
+
+            # work out the area threshold to ignore these crowns in the tiles
+            #tile_width = get_tile_width(file) * scaling[0]
+            #area_threshold = ((tile_width)**2) * area_fraction_limit
+
+            #area_threshold = get_tile_width(file)
+
+            test_file = tile_directory + "/" + file.replace(".geojson", "_geo.geojson")
+            all_test_feats = initialise_feats(test_directory, test_file,
+                                              lidar_img, area_threshold,
+                                              conf_threshold, border_filter,
+                                              tile_width, EPSG)
+
+            pred_file = "Prediction_" + file
+                        all_pred_feats = initialise_feats(pred_directory, pred_file,
+                                              lidar_img, area_threshold,
+                                              conf_threshold, border_filter,
+                                              tile_width, EPSG)
+
+            if save:
+                save_feats(tile_directory, all_test_feats)
+                save_feats(tile_directory, all_pred_feats)
+
+            find_intersections(all_test_feats, all_pred_feats)
+            tps, fps, fns = positives_test(all_test_feats, all_pred_feats,
+                                           IoU_threshold, min_height, max_height)
+
+            print("tps:", tps)
+            print("fps:", fps)
+            print("fns:", fns)
+            print("")
+
+            total_tps += tps
+            total_fps += fps
+            total_fns += fns
+
+    try:
+        prec, rec = prec_recall_func(total_tps, total_fps, total_fns)
+        # not used!
+        f1_score = f1_cal(prec, rec)    # noqa: F841
+        print("Precision  ", "Recall  ", "F1")
+        print(prec, rec, f1_score)
+    except ZeroDivisionError:
+        print("ZeroDivisionError: Height threshold is too large.")
+    return prec, rec, f1_score
 
 if __name__ == "__main__":
     print("to do")
