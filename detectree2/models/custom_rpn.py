b3c709f396c988f1ec121074d03c970f1803cb5f
WangLuran <94464527+WangLuran@users.noreply.github.com>
2022-10-25 10:00:21 +0100

    Update custom_rpn.py

--- detectree2/models/custom_rpn.py
+++ detectree2/models/custom_rpn.py
@@ -43,7 +43,7 @@ def _is_tracing():
         return False
     else:
         return torch.jit.is_tracing()
-  
+
   
 @PROPOSAL_GENERATOR_REGISTRY.register()
 class custom_RPN(RPN):
@@ -173,3 +173,103 @@ class custom_RPN(RPN):
         res.objectness_logits = scores_per_img[keep]
         results.append(res)
     return results
+
+
+def custom_nms(P : torch.tensor ,scores: torch.tensor, thresh_iou : float, thresh_iou_o : float):
+    """
+    Apply non-maximum suppression to avoid detecting too many
+    overlapping bounding boxes for a given object.
+    Args:
+        boxes: (tensor) The location preds for the image 
+            along with the class predscores, Shape: [num_boxes,5].
+        thresh_iou: (float) The overlap thresh for suppressing unnecessary boxes.
+    Returns:
+        A list of filtered boxes index, Shape: [ , 1]
+    """
+ 
+    # we extract coordinates for every 
+    # prediction box present in P
+    x1 = P[:, 0]
+    y1 = P[:, 1]
+    x2 = P[:, 2]
+    y2 = P[:, 3]
+ 
+    # we extract the confidence scores as well
+    scores = scores
+ 
+    # calculate area of every block in P
+    areas = (x2 - x1) * (y2 - y1)
+     
+    # sort the prediction boxes in P
+    # according to their confidence scores
+    order = scores.argsort()
+ 
+    # initialise an empty list for 
+    # filtered prediction boxes
+    keep = []
+     
+ 
+    while len(order) > 0:
+         
+        # extract the index of the 
+        # prediction with highest score
+        # we call this prediction S
+        idx = order[-1]
+ 
+        # push S in filtered predictions list
+        keep.append(idx)
+ 
+        # remove S from P
+        order = order[:-1]
+ 
+        # sanity check
+        if len(order) == 0:
+            break
+         
+        # select coordinates of BBoxes according to 
+        # the indices in order
+        xx1 = torch.index_select(x1,dim = 0, index = order)
+        xx2 = torch.index_select(x2,dim = 0, index = order)
+        yy1 = torch.index_select(y1,dim = 0, index = order)
+        yy2 = torch.index_select(y2,dim = 0, index = order)
+ 
+        # find the coordinates of the intersection boxes
+        xx1 = torch.max(xx1, x1[idx])
+        yy1 = torch.max(yy1, y1[idx])
+        xx2 = torch.min(xx2, x2[idx])
+        yy2 = torch.min(yy2, y2[idx])
+ 
+        # find height and width of the intersection boxes
+        w = xx2 - xx1
+        h = yy2 - yy1
+         
+        # take max with 0.0 to avoid negative w and h
+        # due to non-overlapping boxes
+        w = torch.clamp(w, min=0.0)
+        h = torch.clamp(h, min=0.0)
+ 
+        # find the intersection area
+        inter = w*h
+ 
+        # find the areas of BBoxes according the indices in order
+        rem_areas = torch.index_select(areas, dim = 0, index = order) 
+ 
+        # find the union of every prediction T in P
+        # with the prediction S
+        # Note that areas[idx] represents area of S
+        union = (rem_areas - inter) + areas[idx]
+         
+        # find the IoU of every prediction in P with S
+        IoU = inter / union
+
+        # find the interaction over S
+        IoU_S = inter / areas[idx]
+
+        # find the interaction over prediction
+        IoU_P = inter / areas
+ 
+        # keep the boxes with IoU less than thresh_iou
+        mask = (IoU < thresh_iou)&(IoU_S < thresh_iou_o)&(IoU_P < thresh_iou_o)
+        order = order[mask]
+     
+    return keep
