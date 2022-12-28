9788473b63511861fa5d7d8c10eb0b2ad1b7c982
WangLuran <94464527+WangLuran@users.noreply.github.com>
2022-12-28 22:38:14 +0000

    Update custom_nms.py

--- detectree2/models/custom_nms.py
+++ detectree2/models/custom_nms.py
@@ -30,6 +30,7 @@ from detectron2.modeling.box_regression import Box2BoxTransform, _dense_box_regr
 from detectron2.modeling.matcher import Matcher
 from detectron2.modeling.sampling import subsample_labels
 from detectron2.modeling.proposal_generator.build import PROPOSAL_GENERATOR_REGISTRY
+from detectron2.modeling import build_model
 
 
 @torch.jit.script_if_tracing
@@ -332,3 +333,59 @@ def custom_nms_mask(P : torch.tensor ,scores: torch.tensor, thresh_iou_o : float
         order = order.reshape(-1,1)[mask]
      
     return keep
+
+class DefaultPredictor1:
+    """
+    intersection over area and nms is added here
+    """
+    def __init__(self, cfg):
+        self.cfg = cfg.clone()  # cfg can be modified by model
+        self.model = build_model(self.cfg)
+        self.model.eval()
+        if len(cfg.DATASETS.TEST):
+            self.metadata = MetadataCatalog.get(cfg.DATASETS.TEST[0])
+
+        checkpointer = DetectionCheckpointer(self.model)
+        checkpointer.load(cfg.MODEL.WEIGHTS)
+
+        self.aug = T.ResizeShortestEdge(
+            [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
+        )
+
+        self.input_format = cfg.INPUT.FORMAT
+        assert self.input_format in ["RGB", "BGR"], self.input_format
+
+    def __call__(self, original_image):
+        """
+        Args:
+            original_image (np.ndarray): an image of shape (H, W, C) (in BGR order).
+        Returns:
+            predictions (dict):
+                the output of the model for one image only.
+                See :doc:`/tutorials/models` for details about the format.
+        """
+        with torch.no_grad():  # https://github.com/sphinx-doc/sphinx/issues/4258
+            # Apply pre-processing to image.
+            if self.input_format == "RGB":
+                # whether the model expects BGR inputs or RGB
+                original_image = original_image[:, :, ::-1]
+            height, width = original_image.shape[:2]
+            image = self.aug.get_transform(original_image).apply_image(original_image)
+            image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
+
+            inputs = {"image": image, "height": height, "width": width}
+            predictions = self.model([inputs])[0]
+
+            boxes = predictions['instances'].get_fields()['pred_boxes']
+            scores = predictions['instances'].get_fields()['scores']
+            pred_classes = predictions['instances'].get_fields()['pred_classes']
+            pred_masks = predictions['instances'].get_fields()['pred_masks']
+
+            keep = custom_nms_mask(pred_masks ,scores, thresh_iou_o = 0.3)
+
+            predictions['instances'].get_fields()['pred_boxes'] = boxes[keep]
+            predictions['instances'].get_fields()['scores'] = scores[keep]
+            predictions['instances'].get_fields()['pred_classes'] = pred_classes[keep]
+            predictions['instances'].get_fields()['pred_masks'] = pred_masks[keep]
+
+            return predictions
