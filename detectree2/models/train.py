c03d628dc1281372a50d56fbb711a4d2138948ca
WangLuran <94464527+WangLuran@users.noreply.github.com>
2023-01-13 00:37:07 +0000

    Update train.py

--- detectree2/models/train.py
+++ detectree2/models/train.py
@@ -333,62 +333,6 @@ def build_train_loader(cls, cfg):
         ),
     )
 
-@classmethod
-def test_train(cls, cfg, model, evaluators=None):
-    """
-    Evaluate the given model. The given model is expected to already contain
-    weights to evaluate.
-
-    Args:
-        cfg (CfgNode):
-        model (nn.Module):
-        evaluators (list[DatasetEvaluator] or None): if None, will call
-            :meth:`build_evaluator`. Otherwise, must have the same length as
-            ``cfg.DATASETS.TEST``.
-
-    Returns:
-        dict: a dict of result metrics
-    """
-    logger = logging.getLogger(__name__)
-    if isinstance(evaluators, DatasetEvaluator):
-        evaluators = [evaluators]
-    if evaluators is not None:
-        assert len(cfg.DATASETS.TRAIN) == len(evaluators), "{} != {}".format(
-            len(cfg.DATASETS.TRAIN), len(evaluators)
-        )
-
-    results = OrderedDict()
-    for idx, dataset_name in enumerate(cfg.DATASETS.TRAIN):
-        data_loader = cls.build_test_loader(cfg, dataset_name)
-        # When evaluators are passed in as arguments,
-        # implicitly assume that evaluators can be created before data_loader.
-        if evaluators is not None:
-            evaluator = evaluators[idx]
-        else:
-            try:
-                evaluator = cls.build_evaluator(cfg, dataset_name)
-            except NotImplementedError:
-                logger.warn(
-                    "No evaluator found. Use `DefaultTrainer.test(evaluators=)`, "
-                    "or implement its `build_evaluator` method."
-                )
-                results[dataset_name] = {}
-                continue
-        results_i = inference_on_dataset(model, data_loader, evaluator)
-        results[dataset_name] = results_i
-        if comm.is_main_process():
-            assert isinstance(
-                results_i, dict
-            ), "Evaluator must return a dict on the main process. Got {} instead.".format(
-                results_i
-            )
-            logger.info("Evaluation results for {} in csv format:".format(dataset_name))
-            print_csv_format(results_i)
-
-    if len(results) == 1:
-         results = list(results.values())[0]
-    return results
-
 def get_tree_dicts(directory: str, classes: List[str] = None, classes_at: str = None) -> List[Dict]:
     """Get the tree dictionaries.
     Args:
@@ -599,6 +543,7 @@ def setup_cfg(
     nms_thresh_union = 0.1,
     rpn_nms_thresh = 0,
     nms_thresh_test = 0.2,
+    mask_weight = 1,
     out_dir="/content/drive/Shareddrives/detectree2/train_outputs",
     resize=True,
 ):
@@ -652,6 +597,7 @@ def setup_cfg(
     cfg.INPUT.MIN_SIZE_TRAIN = 1000
     
     cfg.MODEL.BACKBONE.FREEZE_AT = 2
+    cfg.mask_weight = mask_weight
     #cfg.MODEL.ROI_BOX_HEAD.BBOX_REG_LOSS_TYPE = 'diou'
     cfg.MODEL.PROPOSAL_GENERATOR.NAME  = 'custom_RPN'
     cfg.nms_thresh_union = rpn_nms_thresh
