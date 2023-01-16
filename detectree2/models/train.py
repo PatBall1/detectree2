5fe518466e94c80a6ddb39c410c6a0278bd6f5e9
WangLuran <94464527+WangLuran@users.noreply.github.com>
2023-01-16 04:04:51 +0000

    Update train.py

--- detectree2/models/train.py
+++ detectree2/models/train.py
@@ -539,7 +539,6 @@ def setup_cfg(
     nms_thresh_union = 0.1,
     rpn_nms_thresh = 0,
     nms_thresh_test = 0.2,
-    mask_weight = 1,
     out_dir="/content/drive/Shareddrives/detectree2/train_outputs",
     resize=True,
 ):
@@ -592,13 +591,12 @@ def setup_cfg(
     cfg.INPUT.MIN_SIZE_TRAIN = 1000
     
     cfg.MODEL.BACKBONE.FREEZE_AT = 2
-    '''cfg.mask_weight = mask_weight
     #cfg.MODEL.ROI_BOX_HEAD.BBOX_REG_LOSS_TYPE = 'diou'
     cfg.MODEL.PROPOSAL_GENERATOR.NAME  = 'custom_RPN'
     cfg.nms_thresh_union = nms_thresh_union
     cfg.MODEL.RPN.NMS_THRESH = rpn_nms_thresh
     cfg.MODEL.RPN.IOA_THRESHOLDS = 0.4
-    cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = nms_thresh_test'''
+    cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = nms_thresh_test
     
     return cfg
 
