5e468ac45542fc362949bc1cfce0965510cf1b0e
Ball JGC <ball.jgc@gmail.com>
2023-07-26 20:07:45 +0000

    import

--- detectree2/models/outputs.py
+++ detectree2/models/outputs.py
@@ -18,6 +18,7 @@ import pandas as pd
 import pycocotools.mask as mask_util
 import rasterio
 from rasterio.crs import CRS
+from shapely.affinity import scale
 from shapely.geometry import Polygon, box, shape
 from shapely.ops import orient, unary_union
 
