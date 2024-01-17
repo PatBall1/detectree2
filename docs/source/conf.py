6b32a3e1360fb46e446e5d6a0333335364f4ef68
Ball JGC <ball.jgc@gmail.com>
2024-01-17 14:07:51 +0000

    version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,11 +18,11 @@ sys.path.insert(0, os.path.abspath("../detectree2"))
 # -- Project information -----------------------------------------------------
 
 project = "detectree2"
-copyright = "2022, James Ball"
+copyright = "2023, James Ball"
 author = "James Ball"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.7"
+release = "1.0.8"
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="detectree2",
-    version="1.0.7",
+    version="1.0.8",
     author="James G. C. Ball",
     author_email="ball.jgc@gmail.com",
     description="Detectree packaging",
