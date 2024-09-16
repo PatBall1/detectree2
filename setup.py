0fea6b47c6ff05da5af64bed77fd6c866e5e071a
James Ball <jgcb3@cam.ac.uk>
2024-09-16 19:31:59 +0100

    update test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="detectree2",
-    version="1.0.8",
+    version="1.1.0",
     author="James G. C. Ball",
     author_email="ball.jgc@gmail.com",
     description="Detectree packaging",
@@ -20,7 +20,7 @@ setup(
         "pypng",
         "pygeos",
         "shapely",
-        "geopandas",
+        "geopandas>=0.14.4",
         "rasterio==1.3a3",
         "fiona",
         "pycrs",
