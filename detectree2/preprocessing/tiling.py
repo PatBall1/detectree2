906d7d5abced7d588a46e996c114e1f9b5f28f07
Christopher Kotthoff <christopher117@rocketmail.com>
2025-07-16 18:10:16 +0100

    rgb contrast and unsafe access fix

--- detectree2/preprocessing/tiling.py
+++ detectree2/preprocessing/tiling.py
@@ -134,7 +134,8 @@ def process_tile(img_path: str,
                  additional_nodata: List[Any] = [],
                  image_statistics: List[Dict[str, float]] = None,
                  ignore_bands_indices: List[int] = [],
-                 use_convex_mask: bool = True):
+                 use_convex_mask: bool = True,
+                 enhance_rgb_contrast: bool = True):
     """Process a single tile for making predictions.
 
     Args:
@@ -199,7 +200,7 @@ def process_tile(img_path: str,
                     unioned_crowns = overlapping_crowns.union_all()
                 else:
                     unioned_crowns = overlapping_crowns.unary_union
-                convex_mask_tif = rasterio.features.geometry_mask([unioned_crowns.convex_hull.buffer(5)],
+                convex_mask_tif = rasterio.features.geometry_mask([unioned_crowns.convex_hull.buffer(3)],
                                                                   transform=out_transform,
                                                                   invert=True,
                                                                   out_shape=(out_img.shape[1], out_img.shape[2]))
@@ -224,8 +225,18 @@ def process_tile(img_path: str,
                 )
                 return None
 
+            if enhance_rgb_contrast:
+                # rescale image to 1-255 (0 is reserved for nodata)
+                min_vals, max_vals = np.percentile(
+                    out_img.reshape(3, -1)[:, ~nan_mask.reshape(-1).astype(bool)], [0.2, 99.8])
+
+                out_img = (out_img - min_vals) / (max_vals - min_vals) * 254 + 1
+
             # Apply nan mask
             out_img[np.broadcast_to((nan_mask == 1)[None, :, :], out_img.shape)] = 0
+
+            if enhance_rgb_contrast:
+                out_img = np.clip(out_img, 0, 255)
 
             dtype, nodata = dtype_map.get(out_img.dtype, (None, None))
             if dtype is None:
@@ -249,20 +260,20 @@ def process_tile(img_path: str,
             r, g, b = out_img[0], out_img[1], out_img[2]
             rgb = np.dstack((b, g, r))  # Reorder for cv2 (BGRA)
 
-            # Rescale to 0-255 if necessary
-            if np.nanmax(g) > 255:
-                rgb_rescaled = rgb / 65535 * 255
-            else:
-                rgb_rescaled = rgb
+            if not enhance_rgb_contrast:
+                # If not enhancing contrast, ensure the dtype is uint8
+                if dtype_bool:
+                    rgb = rgb.astype(np.uint8)
+                else:
+                    rgb = rgb.astype(np.float32)
+                np.clip(rgb, 0, 255, out=rgb)
 
-            np.clip(rgb_rescaled, 0, 255, out=rgb_rescaled)
-
-            cv2.imwrite(str(out_path_root.with_suffix(".png").resolve()), rgb_rescaled.astype(np.uint8))
+            cv2.imwrite(str(out_path_root.with_suffix(".png").resolve()), rgb.astype(np.uint8))
 
             if overlapping_crowns is not None:
-                return data, out_path_root, overlapping_crowns, minx, miny, buffer
+                return out_transform, out_path_root, overlapping_crowns, minx, miny, buffer
 
-            return data, out_path_root, None, minx, miny, buffer
+            return out_transform, out_path_root, None, minx, miny, buffer
 
     except RasterioIOError as e:
         logger.error(f"RasterioIOError while applying mask {coords}: {e}")
@@ -421,9 +432,9 @@ def process_tile_ms(img_path: str,
             # cv2.imwrite(str(out_path_root.with_suffix(".png").resolve()), rgb)
 
             if overlapping_crowns is not None:
-                return data, out_path_root, overlapping_crowns, minx, miny, buffer
+                return out_transform, out_path_root, overlapping_crowns, minx, miny, buffer
 
-            return data, out_path_root, None, minx, miny, buffer
+            return out_transform, out_path_root, None, minx, miny, buffer
 
     except RasterioIOError as e:
         logger.error(f"RasterioIOError while applying mask {coords}: {e}")
@@ -453,7 +464,9 @@ def process_tile_train(
         additional_nodata: List[Any] = [],
         image_statistics: List[Dict[str, float]] = None,
         ignore_bands_indices: List[int] = [],
-        use_convex_mask: bool = True) -> None:
+        use_convex_mask: bool = True,
+        enhance_rgb_contrast: bool = True
+    ) -> None:
     """Process a single tile for training data.
 
     Args:
@@ -477,7 +490,7 @@ def process_tile_train(
     if mode == "rgb":
         result = process_tile(img_path, out_dir, buffer, tile_width, tile_height, dtype_bool, minx, miny, crs, tilename,
                               crowns, threshold, nan_threshold, mask_gdf, additional_nodata, image_statistics,
-                              ignore_bands_indices, use_convex_mask)
+                              ignore_bands_indices, use_convex_mask, enhance_rgb_contrast)
     elif mode == "ms":
         result = process_tile_ms(img_path, out_dir, buffer, tile_width, tile_height, dtype_bool, minx, miny, crs,
                                  tilename, crowns, threshold, nan_threshold, mask_gdf, additional_nodata,
@@ -487,13 +500,13 @@ def process_tile_train(
         # logger.warning(f"Skipping tile at ({minx}, {miny}) due to insufficient data.")
         return
 
-    data, out_path_root, overlapping_crowns, minx, miny, buffer = result
+    out_transform, out_path_root, overlapping_crowns, minx, miny, buffer = result
 
     if overlapping_crowns is not None and not overlapping_crowns.empty:
         overlapping_crowns = overlapping_crowns.explode(index_parts=True)
         moved = overlapping_crowns.translate(-minx + buffer, -miny + buffer)
-        scalingx = 1 / (data.transform[0])
-        scalingy = -1 / (data.transform[4])
+        scalingx = 1 / (out_transform[0])
+        scalingy = -1 / (out_transform[4])
         moved_scaled = moved.scale(scalingx, scalingy, origin=(0, 0))
 
         if mode == "rgb":
@@ -766,6 +779,7 @@ def tile_data(
     overlapping_tiles: bool = False,
     ignore_bands_indices: List[int] = [],
     use_convex_mask: bool = True,
+    enhance_rgb_contrast: bool = True,
 ) -> None:
     """Tiles up orthomosaic and corresponding crowns (if supplied) into training/prediction tiles.
 
@@ -813,7 +827,7 @@ def tile_data(
     tile_args = [
         (img_path, out_dir, buffer, tile_width, tile_height, dtype_bool, minx, miny, crs, tilename, crowns, threshold,
          nan_threshold, mode, class_column, mask_gdf, additional_nodata, image_statistics, ignore_bands_indices,
-         use_convex_mask) for minx, miny in tile_coordinates
+         use_convex_mask, enhance_rgb_contrast) for minx, miny in tile_coordinates
         if mask_path is None or (mask_path is not None and mask_gdf.intersects(
             box(minx, miny, minx + tile_width, miny + tile_height)  #TODO maybe add to_crs here
         ).any())
