cfdf33c3f09729590f64900502d5bb5f3d2d18b9
James Ball <jgcb3@cam.ac.uk>
2025-10-16 10:11:28 +0100

    Bump version to v2.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2023, James Ball"
 author = "James Ball"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.8"
+release = "2.1.1"
 
 # -- General configuration ---------------------------------------------------
 
