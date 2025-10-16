9b6ea3eb215869fa9384a16b0f8640d46306ee99
James Ball <jgcb3@cam.ac.uk>
2025-10-16 10:11:28 +0100

    Bump version to v2.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="detectree2",
-    version="2.0.1",
+    version="2.1.1",
     author="James G. C. Ball",
     author_email="ball.jgc@gmail.com",
     description="Detectree packaging",
